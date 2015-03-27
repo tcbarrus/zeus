16364ba818c1c689ae497e15bce549f6a6f7fea0
Neil Spackman <n.spackman89@gmail.com>
2015-03-26 18:34:00 -0600

    adjusted the blur issues

--- scripts/generic_settings_modal.js
+++ scripts/generic_settings_modal.js
@@ -15,7 +15,7 @@ function showModal() {
 	document.getElementById("settings-block").style.visibility = "visible";
 	// document.getElementById("background").className = "blur";
 	//add blurring
-	// document.getElementById("shadow").style.visibility = "visible";
+	document.getElementById("shadow").style.visibility = "visible";
 	document.getElementById("content").className = "blur";
 	document.getElementById("navBar").className = "blur";	
 }
@@ -27,7 +27,7 @@ function hideModal() {
 	checkAll('options', false);
 
 	// remove blurring
-	// document.getElementById("shadow").style.visibility = "hidden";
+	document.getElementById("shadow").style.visibility = "hidden";
 	document.getElementById("content").className = "";
 	document.getElementById("navBar").className = "";
 }
