c74e677ff8d5a347c7ba4c24c8e8f6a41cbf97a9
Neil Spackman <n.spackman89@gmail.com>
2015-03-26 18:07:05 -0600

    got the check all/uncheck all to work. Added blurring. Still needs functionality for the check boxes and positioning.

--- scripts/generic_settings_modal.js
+++ scripts/generic_settings_modal.js
@@ -1,9 +1,33 @@
+function checkAll(ID, checktoggle)
+{
+  var checkboxes = new Array(); 
+  var x = document.getElementById(ID);
+  checkboxes = x.getElementsByTagName('input');
+ 
+  for (var i=0; i<checkboxes.length; i++)  {
+    if (checkboxes[i].type == 'checkbox')   {
+      checkboxes[i].checked = checktoggle;
+    }
+  }
+}
+
 function showModal() {
 	document.getElementById("settings-block").style.visibility = "visible";
-	document.getElementById("background").className = "blur";	
+	// document.getElementById("background").className = "blur";
+	//add blurring
+	// document.getElementById("shadow").style.visibility = "visible";
+	document.getElementById("content").className = "blur";
+	document.getElementById("navBar").className = "blur";	
 }
 
 function hideModal() {
 	document.getElementById("settings-block").style.visibility = "hidden";
-	document.getElementById("background").className = "";
+	// document.getElementById("background").className = "";
+	checkAll('nutrients', false);
+	checkAll('options', false);
+
+	// remove blurring
+	// document.getElementById("shadow").style.visibility = "hidden";
+	document.getElementById("content").className = "";
+	document.getElementById("navBar").className = "";
 }
